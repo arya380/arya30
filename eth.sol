a7a7bbb0d8a26472c5899e079275896a25617979
arya380 <66722558+arya380@users.noreply.github.com>
2023-06-26 20:03:25 +0530

    eth.sol

---
+++ eth.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.18;
+contract MyToken {
+
+    // public variables here
+    string public tokenName = "ARK";
+    string public tokenAbbvr = "AK";
+    uint public totalSupply = 0;
+
+
+    // mapping variable here
+    mapping(address=>uint) public balances;
+
+
+    // mint function
+    function mint(address _address, uint _value) public{
+        totalSupply+=_value;
+        balances[_address] += _value;
+    }
+
+
+    // burn function
+    function burn(address _address, uint _value) public{
+        require(balances[_address] > _value, "Cannot burn more than balance tokens");
+        totalSupply-=_value;
+        balances[_address] -= _value;
+    }
+
+}
